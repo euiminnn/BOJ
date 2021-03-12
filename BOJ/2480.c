7fcf65d81483e9cea70435b044009f84ee10ba42
Euimin Chung <euimin@Euimins-MacBook-Air.local>
2021-03-12 22:31:14 +0900

    2530

--- BOJ/2480.c
+++ BOJ/2480.c
@@ -6,7 +6,7 @@
 /*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 21:12:57 by echung            #+#    #+#             */
-/*   Updated: 2021/03/12 21:37:52 by echung           ###   ########.fr       */
+/*   Updated: 2021/03/12 21:52:10 by echung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,11 @@ int main(void)
 		printf("%d", 1000 + 100 * b);
 	else if	(a == c && c != b)
 		printf("%d", 1000 + 100 * c);
-	else if ((a < b < c) || (b < a < c))
+	else if ((a < b && b < c) || (b < a && a < c))
 		printf("%d", 100 * c);
-	else if ((a < c < b) || (c < a < b))
+	else if ((a < c && c < b) || (c < a && a < b))
 		printf("%d", 100 * b);
-	else if ((b < c < a) || (c < b < a))
+	else if ((b < c && c < a) || (c < b && b < a))
 		printf ("%d", 100 * a);
+	return (0);
 }
