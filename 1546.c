13a90f68b5d8c4a6aaac12b08d6f45850717a47d
Euimin Chung <jane3079@korea.ac.kr>
2021-04-14 22:24:54 +0900

    1546 first try -> wrong

---
+++ 1546.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   1546.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: echung <echung@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 21:43:54 by echung            #+#    #+#             */
+/*   Updated: 2021/04/14 22:23:29 by echung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+void	em_avg(int* arr, int n, float big)
+{
+	int i;
+	float sum;
+	i = 0;
+	sum = 0;
+	while (i < n)
+	{
+		sum = sum + arr[i];
+		i++;
+	}
+	printf("result = %f", sum * 100 / big / n);
+}
+
+void	em_big(int* arr, int n)
+{
+	int big;
+	int i;
+
+	big = arr[0];
+
+	while (i < n)
+	{
+		if (arr[i] > big)
+			big = arr[i];
+		i++;
+	}
+	em_avg(arr, n, big);
+}
+
+int		main(void)
+{
+	int n;
+	scanf("%d", &n);
+	int arr[n];
+	int tmp;
+	tmp = 0;
+	while (tmp < n)
+	{
+		scanf("%d", &arr[tmp]);
+		tmp++;
+	}
+	em_big(arr, n);
+	return (0);
+}
