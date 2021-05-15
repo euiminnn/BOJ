2e222b3904af2d1afa8074593cdc5ebaced3030b
Euimin Chung <jane3079@korea.ac.kr>
2021-05-16 00:38:10 +0900

    5430 정확성ok 근데 시간초과 ㅠ

--- py/5430.py
+++ py/5430.py
@@ -8,18 +8,28 @@ while case_ > 0 :
     leng.append(int(input()))
     #arr = list(map(int, input()[1:-1].split(", ")))
     str_ = input()
-    str_ = str_[1:-1].split(',')
-    str_ = list(map(int, str_))
-    arr.append(str_)
+    str_ = str_[1:-1]
+    if len(str_) == 0 :
+        arr.append([])
+    else :
+        str_ = str_.split(',')
+        str_ = list(map(int, str_))
+        arr.append(str_)
     case_ -= 1
 
+#print("cmd=", cmd)
+#print("leng=", leng)
+#print("arr=", arr)
 for i in range(0, case) :
-    for l in range(0, leng[i]) :
+    for l in range(0, len(cmd[i])):
         if cmd[i][l] == 'R' :
             arr[i].reverse()
         elif cmd[i][l] == 'D' :
-            arr[i].popleft()
+            if len(arr[i]) == 0 :
+                print("error")
+                break
+            else :
+                arr[i].pop(0)
+    else:
+        print(arr[i])
 
-print("cmd=", cmd)
-print("leng=", leng)
-print("arr=", arr)
